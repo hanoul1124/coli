cd92c6b7df62274f2e861212f3f5fc2a875e2e93
Creation Yun <creationyun@gmail.com>
2019-11-29 22:32:16 +0900

    Fix bug about url, main, login

--- app/config/urls.py
+++ app/config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('members.web_urls')),
+    path('', include('members.web_url')),
     path('admin/', admin.site.urls),
 ]
 
